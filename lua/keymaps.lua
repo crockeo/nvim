9e12325d41eaa60a7402cc7b85175e296521f76e
Cerek Hillen <chillen@lyft.com>
2021-02-25 11:33:52 -0800

    further progress

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,39 +1,36 @@
-local config = require('config')
-local jump = require('jump')
+local config = require("config")
 
 local function init()
     config.set_keymaps({
         all = {
-            {'<C-a>', '^', {}},
-            {'<C-c>jb', '<cmd> lua require("jump").jump_from_definition()<CR>', {}},
-            {'<C-c>jg', '<cmd> lua require("jump").jump_to_definition()<CR>', {}},
-            {'<C-c>jr', 'normal! lua vim.lsp.buf.references()<CR>', {}},
-            {'<C-e>', '<End>', {}},
-            {'<C-g>', '<ESC>', {}},
-            {'<C-x><Left>', ':tabp<CR>', {}},
-            {'<C-x><Right>', ':tabn<CR>', {}},
-            {'<C-x>1', '<C-w>o', {}},
+            {"<C-a>", "^", {}},
+            {"<C-c>jb", "<cmd> lua require('jump').jump_from_definition()<CR>", {}},
+            {"<C-c>jg", "<cmd> lua require('jump').jump_to_definition()<CR>", {}},
+            {"<C-c>jr", "<Plug>(coc-references)", {}},
+            {"<C-e>", "<End>", {}},
+            {"<C-g>", "<ESC>", {}},
+            {"<C-x>1", "<C-w>o", {}},
         },
         c = {
-            {'<C-g>', '<C-c>', {}},
+            {"<C-g>", "<C-c>", {}},
         },
         i = {
-            {'<C-a>', '<ESC>^i', {}},
-            {'<C-e>', '<End>', {}},
-            {'<C-f>', '<ESC>', {}},
-            {'<C-Space>', '<Plug>(completion_trigger)', {silent = true}},
+            {"<C-a>", "<ESC>^i", {}},
+            {"<C-e>", "<End>", {}},
+            {"<C-f>", "<ESC>", {}},
+            {"<C-Space>", "<Plug>(completion_trigger)", {silent = true}},
         },
         n = {
-            {'<C-c>c', 'gc<Right>', {}},
-            {'<C-c>d', ':NERDTreeToggle<CR>', {}},
-            {'<C-c>k', ':bw<CR>', {}},
-            {'<C-c>s', '<cmd>lua vim.lsp.buf.formatting()<CR>', {}},
-            {'<C-x><C-f>', ':e ', {}},
-            {'<C-x>o', '<C-w>w', {}},
-            {';', ':b#<CR>', {}},
+            {"<C-c>c", "gc<Right>", {}},
+            {"<C-c>d", ":NERDTreeToggle<CR>", {}},
+            {"<C-c>k", ":bw<CR>", {}},
+            {"<C-c>s", "<Plug>(coc-format)", {}},
+            {"<C-x><C-f>", ":e ", {}},
+            {"<C-x>o", "<C-w>w", {}},
+            {";", ":b#<CR>", {}},
         },
         v = {
-            {'<C-c>c', 'gc', {}},
+            {"<C-c>c", "gc", {}},
         },
     })
 end
