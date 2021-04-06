1909682f9481550ac899350a670fa17d8de8f748
Cerek Hillen <chillen@lyft.com>
2021-04-06 14:11:15 -0700

    move colorscheme to lush

---
+++ lua/lush_theme/crockeo.lua
@@ -0,0 +1,75 @@
+local lush = require('lush')
+local hsl = lush.hsl
+
+return lush(function()
+    local background = hsl(24, 17, 17)
+    local foreground = hsl(0, 100, 100)
+    local background_light = background.lighten(20)
+    local background_dark = background.darken(10)
+    local comment = background.lighten(35)
+
+    local highlight_red = hsl(0, 100, 70)
+    local highlight_yellow = hsl(25, 90, 70)
+    local highlight_green = hsl(120, 100, 80)
+    local highlight_cyan = hsl(180, 100, 80)
+    local highlight_blue = hsl(240, 100, 80)
+    local highlight_magenta = hsl(270, 65, 65)
+
+    return {
+        Normal { fg = foreground, bg = background },
+
+        Visual { fg = background, bg = highlight_yellow },
+
+        Pmenu { bg = background_light },
+        PmenuSel { fg = background, bg = highlight_yellow },
+
+        StatusLine { fg = background, bg = background_light },
+        StatusLineNC { fg = StatusLine.fg, bg = StatusLine.bg },
+
+        -- Hidden: NonText, SignColumn
+        NonText { fg = background, bg = background },
+        SignColumn { fg = background, bg = background },
+
+        Comment { fg = comment },
+        LineNr { fg = comment },
+        LspDiagnosticsDefaultError { fg = comment },
+        LspDiagnosticsDefaultWarning { fg = comment },
+        LspDiagnosticsDefaultHint { fg = comment },
+        LspDiagnosticsDefaultInformation { fg = comment },
+
+        Error { fg = highlight_red, bg = "NONE" },
+        ErrorMsg { fg = highlight_red, bg = "NONE" },
+        Todo { fg = highlight_red },
+
+        String { fg = highlight_yellow },
+        Character { fg = highlight_yellow },
+
+        Function { fg = highlight_green },
+
+        Conditional { fg = highlight_cyan },
+        Exception { fg = highlight_cyan },
+        Identifier { fg = highlight_cyan },
+        Keyword { fg = highlight_cyan },
+        Label { fg = highlight_cyan },
+        Repeat { fg = highlight_cyan },
+        Statement { fg = highlight_cyan },
+
+        Boolean { fg = highlight_magenta },
+        Constant { fg = highlight_magenta },
+        Float { fg = highlight_magenta },
+        Number { fg = highlight_magenta },
+        PreProc { fg = highlight_magenta },
+        Type { fg = highlight_magenta },
+        Special { fg = highlight_magenta },
+
+        DiffAdd { fg = highlight_green },
+        GitGutterAdd { fg = DiffAdd.fg },
+        DiffChange { fg = highlight_yellow },
+        GitGutterChange { fg = DiffChange.fg },
+        DiffDelete { fg = highlight_red },
+        GitGutterDelete { fg = DiffDelete.fg },
+        DiffText { fg = foreground, bg = background },
+
+        CocHintSign { fg = comment },
+    }
+end)
