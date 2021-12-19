c9e7b64505e7926307132c600eeecd43b265261e
Cerek Hillen <cerekh@gmail.com>
2021-12-18 21:39:22 -0500

    remove old keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,12 +4,6 @@ local function init()
     config.set_keymaps({
         all = {
             {"<C-a>", "^", {}},
-            {"<C-c>jb", "<cmd> lua require('jump').jump_from_definition()<CR>", {}},
-            {"<C-c>jg", "<cmd> lua require('jump').jump_to_definition()<CR>", {}},
-            {"<C-c>ji", "<cmd> lua require('jump').jump_to_implementation()<CR>", {}},
-            {"<C-c>jr", "<cmd> lua require('jump').jump_to_reference()<CR>", {}},
-            {"<C-c>wb", "<cmd> lua require('jump').jump_from_definition()<CR>", {}},
-            {"<C-c>wh", "<cmd> lua require('jump').jump_to_home()<CR>", {}},
             {"<C-e>", "<End>", {}},
             {"<C-g>", "<ESC>", {}},
             {"<C-x>1", "<C-w>o", {}},
